e5491418fa48b521507cfb52b5f130ab75df3cb3
mehmet demircan <mehmeddemircan@gmail.com>
2023-03-29 21:59:08 +0300

    changed userActions url

--- admin-frontend/src/redux/actions/UserActions.js
+++ admin-frontend/src/redux/actions/UserActions.js
@@ -22,7 +22,13 @@ import {
   UPDATE_USER_ROLE_REQUEST,
   UPDATE_USER_ROLE_SUCCESS,
 } from "../constants/UserConstants";
-import { GET_USERS_LOCATIONS_FAIL, GET_USERS_LOCATIONS_REQUEST, GET_USERS_LOCATIONS_SUCCESS, UPDATE_LIVE_LOCATION_FAIL, UPDATE_LIVE_LOCATION_SUCCESS } from "../constants/LocationConstants";
+import {
+  GET_USERS_LOCATIONS_FAIL,
+  GET_USERS_LOCATIONS_REQUEST,
+  GET_USERS_LOCATIONS_SUCCESS,
+  UPDATE_LIVE_LOCATION_FAIL,
+  UPDATE_LIVE_LOCATION_SUCCESS,
+} from "../constants/LocationConstants";
 
 export const AllUser = (limit, page) => async (dispatch) => {
   try {
@@ -30,7 +36,9 @@ export const AllUser = (limit, page) => async (dispatch) => {
       type: GET_ALL_USER_REQUEST,
     });
 
-    const { data } = await axios.get(`https://afetapi.onrender.com/api/users?limit=${limit}&page=${page}`);
+    const { data } = await axios.get(
+      `https://afetapi.onrender.com/api/users?limit=${limit}&page=${page}`
+    );
 
     dispatch({
       type: GET_ALL_USER_SUCCESS,
@@ -66,163 +74,161 @@ export const SearchUser = (name) => async (dispatch) => {
   }
 };
 
-
 export const UpdateUserRole = (id) => async (dispatch) => {
-    try {
-      dispatch({
-        type: UPDATE_USER_ROLE_REQUEST,
-      });
-  
-      const { data } = await axios.put(
-        `https://afetapi.onrender.com/api/users/${id}/update-role`
-      );
-  
-      dispatch({
-        type: UPDATE_USER_ROLE_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: UPDATE_USER_ROLE_FAIL,
-        error: error.response,
-      });
-    }
-  };
-  
+  try {
+    dispatch({
+      type: UPDATE_USER_ROLE_REQUEST,
+    });
 
-  export const GetAllUserLocations = () => async (dispatch) => {
-    try {
-      dispatch({
-        type: GET_USERS_LOCATIONS_REQUEST,
-      });
-  
-      const { data } = await axios.get(
-        `https://afetapi.onrender.com/api/map/users`
+    const { data } = await axios.put(
+      `https://afetapi.onrender.com/api/users/${id}/update-role`
+    );
 
-      );
-  
-      dispatch({
-        type: GET_USERS_LOCATIONS_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: GET_USERS_LOCATIONS_FAIL,
-        error: error.response,
-      });
-    }
-  };
-  
-  export const UpdateLiveLocation = (userId, lat, lng) => async (dispatch) => {
-    try {
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-  
-      const { data } = await axios.patch(
-        `https://afetapi.onrender.com/api/users/${userId}/location`,
-        { lat, lng },
-        config
-      );
-  
-      dispatch({
-        type: UPDATE_LIVE_LOCATION_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: UPDATE_LIVE_LOCATION_FAIL,
-        payload: error.response.data.message,
-      });
-    }
-  };
+    dispatch({
+      type: UPDATE_USER_ROLE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_USER_ROLE_FAIL,
+      error: error.response,
+    });
+  }
+};
 
+export const GetAllUserLocations = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_USERS_LOCATIONS_REQUEST,
+    });
 
-  export const AddTaskToUser = (userId,task) => async (dispatch) => {
-    try {
-      dispatch({
-        type: ADD_TASK_TO_USER_REQUEST,
-      });
-  
-      const { data } = await axios.post(
-        `http://localhost:5000/api/users/${userId}/add-task`,task
-      );
-  
-      dispatch({
-        type: ADD_TASK_TO_USER_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: ADD_TASK_TO_USER_FAIL,
-        error: error.response,
-      });
-    }
-  };
-  
+    const { data } = await axios.get(
+      `https://afetapi.onrender.com/api/map/users`
+    );
 
-  export const RemoveTaskFromUser = (userId,taskId) => async (dispatch) => {
-    try {
-      dispatch({
-        type: REMOVE_TASK_TO_USER_REQUEST,
-      });
-  
-      const { data } = await axios.delete(
-        `http://localhost:5000/api/users/${userId}/tasks/${taskId}/remove`
-      );
-  
-      dispatch({
-        type: REMOVE_TASK_TO_USER_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: REMOVE_TASK_TO_USER_FAIL,
-        error: error.response,
-      });
-    }
-  };
+    dispatch({
+      type: GET_USERS_LOCATIONS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_USERS_LOCATIONS_FAIL,
+      error: error.response,
+    });
+  }
+};
 
-  export const GetUserTasks = (userId) => async (dispatch) => {
-    try {
-      dispatch({
-        type: GET_USER_TASKS_REQUEST,
-      });
-  
-      const { data } = await axios.get(`http://localhost:5000/api/users/${userId}/tasks`);
-  
-      dispatch({
-        type: GET_USER_TASKS_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: GET_USER_TASKS_FAIL,
-        error: error.response,
-      });
-    }
-  };
+export const UpdateLiveLocation = (userId, lat, lng) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-  export const GetAllTaskNotAdded =  (userId) => async (dispatch) => {
-    try {
-      dispatch({
-        type: GET_NOT_ADDED_TASK_REQUEST,
-      });
-  
-      const { data } = await axios.get(
-        `https://afetapi.onrender.com/api/users/${userId}/not-added-tasks`
-      );
-  
-      dispatch({
-        type: GET_NOT_ADDED_TASK_SUCCESS,
-        payload: data,
-      });
-    } catch (error) {
-      dispatch({
-        type: GET_NOT_ADDED_TASK_FAIL,
-        error: error.response,
-      });
-    }
-  };
+    const { data } = await axios.patch(
+      `https://afetapi.onrender.com/api/users/${userId}/location`,
+      { lat, lng },
+      config
+    );
+
+    dispatch({
+      type: UPDATE_LIVE_LOCATION_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_LIVE_LOCATION_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+export const AddTaskToUser = (userId, task) => async (dispatch) => {
+  try {
+    dispatch({
+      type: ADD_TASK_TO_USER_REQUEST,
+    });
+
+    const { data } = await axios.post(
+      `https://afetapi.onrender.com/api/users/${userId}/add-task`,
+      task
+    );
+
+    dispatch({
+      type: ADD_TASK_TO_USER_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ADD_TASK_TO_USER_FAIL,
+      error: error.response,
+    });
+  }
+};
+
+export const RemoveTaskFromUser = (userId, taskId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: REMOVE_TASK_TO_USER_REQUEST,
+    });
+
+    const { data } = await axios.delete(
+      `https://afetapi.onrender.com/api/users/${userId}/tasks/${taskId}/remove`
+    );
+
+    dispatch({
+      type: REMOVE_TASK_TO_USER_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: REMOVE_TASK_TO_USER_FAIL,
+      error: error.response,
+    });
+  }
+};
+
+export const GetUserTasks = (userId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_USER_TASKS_REQUEST,
+    });
+
+    const { data } = await axios.get(
+      `https://afetapi.onrender.com/api/users/${userId}/tasks`
+    );
+
+    dispatch({
+      type: GET_USER_TASKS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_USER_TASKS_FAIL,
+      error: error.response,
+    });
+  }
+};
+
+export const GetAllTaskNotAdded = (userId) => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_NOT_ADDED_TASK_REQUEST,
+    });
+
+    const { data } = await axios.get(
+      `https://afetapi.onrender.com/api/users/${userId}/not-added-tasks`
+    );
+
+    dispatch({
+      type: GET_NOT_ADDED_TASK_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_NOT_ADDED_TASK_FAIL,
+      error: error.response,
+    });
+  }
+};
