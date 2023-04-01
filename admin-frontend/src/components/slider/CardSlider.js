b3119711a97991b111c2fc5de24d42060928eea3
mehmet demircan <mehmeddemircan@gmail.com>
2023-04-02 00:00:40 +0300

    Card Slider added to home page

---
+++ admin-frontend/src/components/slider/CardSlider.js
@@ -0,0 +1,313 @@
+import { Card, Carousel } from "antd";
+import React from "react";
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+const { Meta } = Card;
+
+const settings = {
+  dots: false,
+  arrows: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+};
+const CardSlider = () => {
+  return (
+    <Fragment>
+      <Carousel
+        autoplay
+        {...settings}
+        style={{ marginTop: 10, height: 280, padding: "0px 0px" }}
+      >
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+
+        <Link>
+          <Card
+            hoverable
+            style={{ width: 240, borderRadius: 20, marginBottom: 10 }}
+            cover={
+              <img
+                style={{
+                  height: 160,
+                  objectFit: "cover",
+                  borderTopLeftRadius: 20,
+                  borderTopRightRadius: 20,
+                }}
+                alt="product name"
+                src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=2560"
+              />
+            }
+          >
+            <Meta
+              title="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+              description={<h5>Special homes</h5>}
+            />
+          </Card>
+        </Link>
+      </Carousel>
+    </Fragment>
+  );
+};
+
+export default CardSlider;
--- admin-frontend/src/pages/HomePage.js
+++ admin-frontend/src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MainLayout from "../components/layout/MainLayout";
 import HomeJumbotron from "../components/jumbotron/HomeJumbotron";
+import CardSlider from "../components/slider/CardSlider";
 
 const HomePage = () => {
   return (
     <MainLayout>
       <HomeJumbotron />
+      <CardSlider />
     </MainLayout>
   );
 };
