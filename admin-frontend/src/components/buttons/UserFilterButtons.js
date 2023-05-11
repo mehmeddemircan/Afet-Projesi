aeb3ee3a0cd2c625d031b7a5893448ec01ccc3f7
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-11 17:34:54 +0300

    removed unnecessary buttons

--- admin-frontend/src/components/buttons/UserFilterButtons.js
+++ admin-frontend/src/components/buttons/UserFilterButtons.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react'
 
-const UserFilterButtons = () => {
+const UserFilterButtons = ({handleToggleShowLiveLocationUsers}) => {
   return (
     <Fragment>
 
         <div className='row mb-3'>
             <div className='d-flex justify-content-end'>
-                <button className='btn btn-outline-secondary rounded-pill mx-2'>Live Location</button>
+                <button className='btn btn-outline-secondary rounded-pill mx-2' onClick={handleToggleShowLiveLocationUsers}>Live Location</button>
              
             </div>
         </div>
