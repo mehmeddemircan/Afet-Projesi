e1713901253b1dc76f2aafdaa088abbc67325eae
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-13 15:52:59 +0300

    when refresh the page , prevent logout,added pages

--- user-frontend/src/App.js
+++ user-frontend/src/App.js
@@ -1,14 +1,29 @@
-
-import { BrowserRouter as Router ,Routes, Route} from 'react-router-dom'
-import HomePage from './pages/HomePage';
-
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import AuthPage from "./pages/AuthPage";
+import { isUserLoggedIn } from "./redux/actions/AuthActions";
+import {useSelector, useDispatch} from 'react-redux'
 function App() {
+
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+ 
+ // When we fresh the page if you are in logged in  stay logged in
+  useEffect(() => {
+    if (!auth.authenticate) {
+      dispatch(isUserLoggedIn());
+    }
+  }, [auth.authenticate]);
   return (
-      <Router>
-        <Routes>
-          <Route index path='/' element={<HomePage />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
