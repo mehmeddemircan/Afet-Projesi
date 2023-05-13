a65e94a4ab46a3904e1a11388a44797f7034ad0e
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-13 22:54:56 +0300

    created loading spinner

---
+++ user-frontend/src/components/spinner/LoadingSpinner.js
@@ -0,0 +1,10 @@
+import { Spin } from 'antd';
+import React from 'react'
+const LoadingSpinner = () => {
+    return (
+        <div className='d-flex justify-content-center align-items-center' style={{width : '100%'}}>
+             <Spin size='large' />
+        </div>
+    )
+};
+export default LoadingSpinner;
