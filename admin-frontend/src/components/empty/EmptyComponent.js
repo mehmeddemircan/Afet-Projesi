0fa3b1ac93c8d4f6da220b1c6cbd8ede1f1b1648
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-13 22:53:14 +0300

    changed empty component

--- admin-frontend/src/components/empty/EmptyComponent.js
+++ admin-frontend/src/components/empty/EmptyComponent.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { Empty } from 'antd';
+import React from "react";
+import { Empty } from "antd";
 const EmptyComponent = () => {
   return (
-   
-      <div className='d-flex justify-content-center align-items-center' style={{width:'100vw'}}>
-        <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-      </div>
-   
-  )
-}
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ width: "100%" }}
+    >
+      <Empty
+        description={<h4>Maalesef herhangi bir kayıt bulamadık</h4>}
+        image="https://img.freepik.com/free-vector/empty-concept-illustration_114360-7416.jpg"
+        imageStyle={{ width: "50%", height: "50%" }}
+      />
+    </div>
+  );
+};
 
-export default EmptyComponent
+export default EmptyComponent;
