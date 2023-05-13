f024236ba75c4d76f390a8450acae21774ecb188
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-13 15:55:42 +0300

    created meta title to user frontend

---
+++ user-frontend/src/meta/MetaTitle.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Fragment } from 'react'
+import { Helmet } from 'react-helmet'
+
+const MetaTitle = ({title,name,content}) => {
+  return (
+    <Helmet>
+        <title>{title}</title>
+    <meta name={name} content={content} />
+    </Helmet>
+  )
+}
+
+export default MetaTitle
