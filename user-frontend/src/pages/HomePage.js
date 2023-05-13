5b35b137472f100037c61217d270f1a27170e6cc
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-13 22:55:23 +0300

    added brand category card

--- user-frontend/src/pages/HomePage.js
+++ user-frontend/src/pages/HomePage.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {Button} from 'antd'
 import MainLayout from '../components/layout/MainLayout'
 import MetaTitle from '../meta/MetaTitle'
+import BrandCategoryCard from '../components/card/BrandCategoryCard'
 const HomePage = () => {
   return (
       <MainLayout>
         <MetaTitle title="Anasayfa" name="Anasayfa" content="Anasayfa" />
-          <Button>HomePage</Button>
+          <BrandCategoryCard />
       </MainLayout>
   )
 }
