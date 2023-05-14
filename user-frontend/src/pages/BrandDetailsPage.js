e606ba8741455f82d2de098ceeb1545b195e8451
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-14 12:43:43 +0300

    added info bread crumb to page

--- user-frontend/src/pages/BrandDetailsPage.js
+++ user-frontend/src/pages/BrandDetailsPage.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { AllClothesByBrand, AllMealByBrand, AllShelterByBrand, GetSingleBrand } from '../redux/actions/BrandActions'
 import MainLayout from '../components/layout/MainLayout'
 import BrandProductList from '../components/list/BrandProductList'
+import InfoBreadcrumb from '../components/breadcrumb/InfoBreadCrumb'
 
 const BrandDetailsPage = () => {
 
     const {id} = useParams()
     const dispatch = useDispatch()
-
+    const navigate = useNavigate()
     const getSingleBrand = useSelector((state) => state.brand.getSingleBrand)
 
     useEffect(() => {
@@ -30,6 +31,41 @@ const BrandDetailsPage = () => {
 
   return (
         <MainLayout>
+             
+        <InfoBreadcrumb
+          items={[
+            {
+              title: "Home",
+            },
+            {
+              title: (
+                <a href={`/kategoriler/${getSingleBrand.brand.category}`}>
+                  {getSingleBrand.brand.category} Markaları
+                </a>
+              ),
+            },
+            {
+              title: (
+                <>
+                  <a onClick={(e) => e.preventDefault()}>
+                    {getSingleBrand.brand.name}
+                  </a>
+                </>
+              ),
+            },
+          ]}
+        />
+
+      <h4>{getSingleBrand.brand.name}</h4>
+      <div className="d-flex flex-row justify-content-between">
+        <button
+          className="btn btn-light"
+          onClick={() => navigate(`/kategoriler/${getSingleBrand.brand.category}`, { replace: true })}
+        >
+          <i class="fa-solid fa-angle-left"></i> Geri
+        </button>
+        </div>
+       
             <BrandProductList />
         </MainLayout>
   )
