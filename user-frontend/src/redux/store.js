fe78196e3a8a1c438e8f42516a2aed819121ca0a
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-14 12:44:26 +0300

    added area , form ,city country reducers

--- user-frontend/src/redux/store.js
+++ user-frontend/src/redux/store.js
@@ -3,12 +3,18 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { authReducer } from "./reducers/AuthReducer";
 import brandReducer from "./reducers/BrandReducer";
+import areaReducer from "./reducers/AreaReducer";
+import formReducer from "./reducers/FormReducer";
+import cityCountryReducer from "./reducers/CityCountryReducer";
 
 
 
 const rootReducer = combineReducers({
     auth : authReducer,
-    brand : brandReducer 
+    brand : brandReducer ,
+    area : areaReducer , 
+    form : formReducer,
+    cityCountry :  cityCountryReducer
 })
 
 const initialState = {}
