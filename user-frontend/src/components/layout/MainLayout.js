6ba3c1423534cbc6dff17b2dcf2efb1125fa32bc
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-14 12:41:28 +0300

    added show map button and backtop button

--- user-frontend/src/components/layout/MainLayout.js
+++ user-frontend/src/components/layout/MainLayout.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Fragment } from 'react'
 import MainHeader from '../header/MainHeader'
 import MainFooter from '../footer/MainFooter'
+import BackTopButton from '../backtop/BackTopButton'
+import ShowMapButton from '../floatbutton/ShowMapButton'
+import GoogleMapComp from '../map/GoogleMapComp'
+import { useDispatch } from 'react-redux'
+import { AllArea } from '../../redux/actions/AreaActions'
 
 const MainLayout = ({children}) => {
+
+  const [showMap, setShowMap] = useState(false);
+  const dispatch = useDispatch()
+  const handleToggleShowMap = () => {
+    setShowMap((prev) => !prev);
+    if (!showMap) {
+      dispatch(AllArea([],[],[]))
+    }
+  };
+
   return (
         <Fragment>
             <MainHeader />
-            <div className='container'>{children}</div>
+
+            {showMap ? (
+        <>
+          <GoogleMapComp />
+        </>
+      ) : (
+        <div className="container">{children}</div>
+      )}
+      <ShowMapButton
+        handleToggleShowMap={handleToggleShowMap}
+        showMap={showMap}
+      />
+         <BackTopButton />
             <MainFooter />
         </Fragment>
   )
