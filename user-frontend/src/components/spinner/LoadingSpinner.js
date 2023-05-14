188d6b50eb5d22785c2496225afacce7da6e5418
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-14 12:43:05 +0300

    created map loading spinnet to align center

--- user-frontend/src/components/spinner/LoadingSpinner.js
+++ user-frontend/src/components/spinner/LoadingSpinner.js
@@ -2,9 +2,9 @@ import { Spin } from 'antd';
 import React from 'react'
 const LoadingSpinner = () => {
     return (
-        <div className='d-flex justify-content-center align-items-center' style={{width : '100%'}}>
+         <div className='d-flex justify-content-center align-items-center' style={{width : '100%'}}>
              <Spin size='large' />
-        </div>
+       </div>
     )
 };
 export default LoadingSpinner;
---
+++ user-frontend/src/components/spinner/MapLoadingSpinner.js
@@ -0,0 +1,10 @@
+import { Spin } from 'antd'
+import React from 'react'
+
+const MapLoadingSpinner = () => {
+  return (
+    <Spin size='large' />
+  )
+}
+
+export default MapLoadingSpinner
