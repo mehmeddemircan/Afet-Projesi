85139a9268adc99b31704d31984b57063ffd2999
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-15 14:16:24 +0300

    added meal form id and remove when deleted

--- backend/controllers/mealNeedForm.js
+++ backend/controllers/mealNeedForm.js
@@ -1,9 +1,19 @@
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const MealNeedForm = require("../models/MealNeedForm");
+const User = require("../models/User");
 
 exports.createMealNeedForm = catchAsyncErrors(async (req, res) => {
   try {
-    await MealNeedForm.create(req.body);
+    const mealNeedForm = await MealNeedForm.create(req.body);
+
+          // Add form ID to user model's clothingForms array
+ const user = await User.findById(req.body.userId);
+ if (user) {
+   user.mealForms.push(mealNeedForm._id);
+   await user.save();
+ }
+
+
     res.status(200).json({ message: "Successfully sent it " });
   } catch (error) {
     res.status(500).json(error);
@@ -61,6 +71,10 @@ exports.updateMealNeedForm = catchAsyncErrors(async (req, res) => {
 exports.deleteMealNeedForm = catchAsyncErrors(async (req, res) => {
   try {
     await MealNeedForm.findByIdAndDelete(req.params.id);
+    const userId = req.params.userId; // Assuming you have a middleware that sets the authenticated user in req.user
+     await User.findByIdAndUpdate(userId, {
+       $pull: { mealForms: req.params.id }
+     });
 
     res.status(200).json({
       message: "Successfully deleted meal need form ",
@@ -88,3 +102,25 @@ exports.approveMealNeedForm = catchAsyncErrors(async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+
+exports.getUserMealForms = catchAsyncErrors(async(req,res) => {
+  try {
+
+    const user = await User.findById(req.params.userId).populate("mealForms")
+    res.status(200).json({ mealForms : user.mealForms})
+  } catch (error) {
+    res.status(500).json({error : error.message})
+  }
+})
+
+
+exports.getUserMealFormsLength = catchAsyncErrors(async(req,res) => {
+  try {
+
+    const user = await User.findById(req.params.userId)
+    res.status(200).json({ length : user.mealForms.length})
+  } catch (error) {
+    res.status(500).json({error : error.message})
+  }
+})
