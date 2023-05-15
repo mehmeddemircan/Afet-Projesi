1f7b32f8fc864e4f0ef84e9372f99eac9d8add61
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-15 14:16:13 +0300

    added clothes form id to user clothing forms

--- backend/controllers/clothingNeedForm.js
+++ backend/controllers/clothingNeedForm.js
@@ -1,9 +1,18 @@
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const ClothingNeedForm = require("../models/ClothingNeedForm");
-
+const User = require ('../models/User')
 exports.createClothingNeedForm = catchAsyncErrors(async (req, res) => {
   try {
     const clothingNeedForm = await ClothingNeedForm.create(req.body);
+
+     // Add form ID to user model's clothingForms array
+ const user = await User.findById(req.body.userId);
+ if (user) {
+   user.clothingForms.push(clothingNeedForm._id);
+   await user.save();
+ }
+
+
     res.status(201).json({ message: "Successfully sent it and created" });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -58,14 +67,24 @@ exports.updateClothingNeedForm = catchAsyncErrors(async (req, res) => {
   }
 });
 
+
+
 exports.deleteClothingNeedForm = catchAsyncErrors(async (req, res) => {
   try {
-    await ClothingNeedForm.findByIdAndDelete(req.params.id);
+     await ClothingNeedForm.findByIdAndDelete(req.params.id);
+     const userId = req.params.userId; // Assuming you have a middleware that sets the authenticated user in req.user
+     await User.findByIdAndUpdate(userId, {
+       $pull: { clothingForms: req.params.id }
+     });
 
     res.status(200).json({
       message: "Successfully deleted clothing need form ",
     });
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({
+      error : error.message
+    })
+  }
 });
 
 
@@ -86,5 +105,27 @@ exports.approveClothingForm = catchAsyncErrors(async(req,res) => {
 
   } catch (error) {
     res.status(500).json(error)    
+  }
+})
+
+
+exports.getUserClothingForms = catchAsyncErrors(async(req,res) => {
+  try {
+
+    const user = await User.findById(req.params.userId).populate("clothingForms")
+    res.status(200).json({ clothingForms : user.clothingForms})
+  } catch (error) {
+    res.status(500).json({error : error.message})
+  }
+})
+
+
+exports.getUserClothingFormsLength = catchAsyncErrors(async(req,res) => {
+  try {
+
+    const user = await User.findById(req.params.userId)
+    res.status(200).json({ length : user.clothingForms.length})
+  } catch (error) {
+    res.status(500).json({error : error.message})
   }
 })
