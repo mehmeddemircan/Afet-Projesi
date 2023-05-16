6c8e8aed0e1aedcd9ef8e375bca3a0639773df67
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-16 23:00:11 +0300

    added all country action

--- user-frontend/src/redux/actions/CityCountryActions.js
+++ user-frontend/src/redux/actions/CityCountryActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ALL_CITY_FAIL, GET_ALL_CITY_REQUEST, GET_ALL_CITY_SUCCESS } from "../constants/CityCountryConstants";
+import { GET_ALL_CITY_FAIL, GET_ALL_CITY_REQUEST, GET_ALL_CITY_SUCCESS, GET_ALL_COUNTRY_FAIL, GET_ALL_COUNTRY_REQUEST, GET_ALL_COUNTRY_SUCCESS } from "../constants/CityCountryConstants";
 
 export const AllCity = (countryId) => async (dispatch) => {
     try {
@@ -22,3 +22,24 @@ export const AllCity = (countryId) => async (dispatch) => {
       });
     }
   };
+
+  
+export const AllCountry = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_ALL_COUNTRY_REQUEST,
+    });
+
+    const { data } = await axios.get(`https://afetapi.onrender.com/api/countries`);
+
+    dispatch({
+      type: GET_ALL_COUNTRY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ALL_COUNTRY_FAIL,
+      error: error.response,
+    });
+  }
+};
