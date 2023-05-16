7971606ae4c4dee232431da98eb5fe277ad7b80a
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-16 23:00:48 +0300

    get task filter by city action added

--- user-frontend/src/redux/actions/TaskActions.js
+++ user-frontend/src/redux/actions/TaskActions.js
@@ -1,4 +1,4 @@
-import { GET_USER_TASKS_FAIL, GET_USER_TASKS_REQUEST, GET_USER_TASKS_SUCCESS } from "../constants/TaskConstants";
+import { GET_TASKS_BY_CITY_FAIL, GET_TASKS_BY_CITY_REQUEST, GET_TASKS_BY_CITY_SUCCESS, GET_USER_TASKS_FAIL, GET_USER_TASKS_REQUEST, GET_USER_TASKS_SUCCESS } from "../constants/TaskConstants";
 import axios from 'axios'
 export const GetUserTasks = (userId) => async (dispatch) => {
     try {
@@ -21,3 +21,26 @@ export const GetUserTasks = (userId) => async (dispatch) => {
       });
     }
   };
+
+  export const GetTasksByCityId = (cityIds) => async (dispatch) => {
+    try {
+      dispatch({
+        type: GET_TASKS_BY_CITY_REQUEST,
+      });
+  
+      const { data } = await axios.get(
+        `https://afetapi.onrender.com/api/get-tasks-by-city?cityIds=${cityIds}`
+      );
+  
+      dispatch({
+        type: GET_TASKS_BY_CITY_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_TASKS_BY_CITY_FAIL,
+        error: error.response,
+      });
+    }
+  };
+  
