5b1a4c5c5c4ef9c3537457b7bd072b1fd42c8925
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-16 22:59:21 +0300

    created city country select input

---
+++ user-frontend/src/components/select/CityCountrySelect.js
@@ -0,0 +1,66 @@
+import { Select, Space } from "antd";
+import React from "react";
+import { useSelector } from "react-redux";
+const { Option } = Select;
+const CityCountrySelect = ({
+  handleAddCityChange,
+  handleCountryChange,
+  isMultipleSelect,
+  handleCityChange,
+}) => {
+  const getAllCountry = useSelector((state) => state.cityCountry.getAllCountry);
+  const getAllCity = useSelector((state) => state.cityCountry.getAllCity);
+  return (
+    <Space
+      style={{
+        width: "300px",
+      }}
+      direction="vertical"
+    >
+      <label className="col-form-label">Country</label>
+      <Select
+        allowClear
+        style={{
+          width: "100%",
+        }}
+        placeholder="Please select country"
+        onChange={handleCountryChange}
+      >
+        {getAllCountry.countries.map((country) => (
+          <Option value={country._id}>{country.name}</Option>
+        ))}
+      </Select>
+      <label className="col-form-label">Cities</label>
+      {isMultipleSelect ? (
+        <Select
+          mode="multiple"
+          allowClear
+          style={{
+            width: "100%",
+          }}
+          placeholder="Please select"
+          onChange={handleAddCityChange}
+        >
+          {getAllCity.cities.map((city) => (
+            <Option value={city._id}>{city.name}</Option>
+          ))}
+        </Select>
+      ) : (
+        <Select
+          allowClear
+          style={{
+            width: "100%",
+          }}
+          placeholder="Please select city "
+          onChange={handleCityChange}
+        >
+          {getAllCity.cities.map((city) => (
+            <Option value={city._id}>{city.name}</Option>
+          ))}
+        </Select>
+      )}
+    </Space>
+  );
+};
+
+export default CityCountrySelect;
