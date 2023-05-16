a6e2210693bf46a8a86b1d057e8b699f230d1c7d
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-16 16:16:18 +0300

    changed clothing need form controller

--- backend/controllers/clothingNeedForm.js
+++ backend/controllers/clothingNeedForm.js
@@ -1,17 +1,16 @@
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const ClothingNeedForm = require("../models/ClothingNeedForm");
-const User = require ('../models/User')
+const User = require("../models/User");
 exports.createClothingNeedForm = catchAsyncErrors(async (req, res) => {
   try {
     const clothingNeedForm = await ClothingNeedForm.create(req.body);
 
-     // Add form ID to user model's clothingForms array
- const user = await User.findById(req.body.userId);
- if (user) {
-   user.clothingForms.push(clothingNeedForm._id);
-   await user.save();
- }
-
+    // Add form ID to user model's clothingForms array
+    const user = await User.findById(req.body.userId);
+    if (user) {
+      user.clothingForms.push(clothingNeedForm._id);
+      await user.save();
+    }
 
     res.status(201).json({ message: "Successfully sent it and created" });
   } catch (error) {
@@ -67,65 +66,61 @@ exports.updateClothingNeedForm = catchAsyncErrors(async (req, res) => {
   }
 });
 
-
-
 exports.deleteClothingNeedForm = catchAsyncErrors(async (req, res) => {
   try {
-     await ClothingNeedForm.findByIdAndDelete(req.params.id);
-     const userId = req.params.userId; // Assuming you have a middleware that sets the authenticated user in req.user
-     await User.findByIdAndUpdate(userId, {
-       $pull: { clothingForms: req.params.id }
-     });
+    await ClothingNeedForm.findByIdAndDelete(req.params.id);
+    const userId = req.params.userId; // Assuming you have a middleware that sets the authenticated user in req.user
+    await User.findByIdAndUpdate(userId, {
+      $pull: { clothingForms: req.params.id },
+    });
 
     res.status(200).json({
       message: "Successfully deleted clothing need form ",
     });
   } catch (error) {
     res.status(500).json({
-      error : error.message
-    })
+      error: error.message,
+    });
   }
 });
 
-
-exports.approveClothingForm = catchAsyncErrors(async(req,res) => {
+exports.approveClothingForm = catchAsyncErrors(async (req, res) => {
   try {
-       await ClothingNeedForm.findByIdAndUpdate(req.params.id, {
-        $set : {
-          isApproved : true ,
-          ...req.body
-        }
+    await ClothingNeedForm.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          isApproved: true,
+          ...req.body,
+        },
       },
-        {new : true}
-      )
+      { new: true }
+    );
 
-      res.status(200).json({
-        message : "Successfully approved clothing form"
-      })
-
+    res.status(200).json({
+      message: "Successfully approved clothing form",
+    });
   } catch (error) {
-    res.status(500).json(error)    
+    res.status(500).json(error);
   }
-})
+});
 
-
-exports.getUserClothingForms = catchAsyncErrors(async(req,res) => {
+exports.getUserClothingForms = catchAsyncErrors(async (req, res) => {
   try {
-
-    const user = await User.findById(req.params.userId).populate("clothingForms")
-    res.status(200).json({ clothingForms : user.clothingForms})
+    const user = await User.findById(req.params.userId).populate(
+      "clothingForms"
+    );
+    res.status(200).json( user.clothingForms );
   } catch (error) {
-    res.status(500).json({error : error.message})
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
-
-exports.getUserClothingFormsLength = catchAsyncErrors(async(req,res) => {
+exports.getUserClothingFormsLength = catchAsyncErrors(async (req, res) => {
   try {
-
-    const user = await User.findById(req.params.userId)
-    res.status(200).json({ length : user.clothingForms.length})
+    const user = await User.findById(req.params.userId);
+    res.status(200).json({ length: user.clothingForms.length });
   } catch (error) {
-    res.status(500).json({error : error.message})
+    res.status(500).json({ error: error.message });
   }
-})
+});
