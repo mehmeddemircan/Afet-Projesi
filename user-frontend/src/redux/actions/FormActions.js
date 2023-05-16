3288e0c34f108b394eca1f51274ba42ebee4cf2f
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-16 16:20:25 +0300

    created actions to list forms for each

--- user-frontend/src/redux/actions/FormActions.js
+++ user-frontend/src/redux/actions/FormActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_FORM_CATEGORIES_FAIL, GET_FORM_CATEGORIES_REQUEST, GET_FORM_CATEGORIES_SUCCESS, GET_SINGLE_FORM_CATEGORY_FAIL, GET_SINGLE_FORM_CATEGORY_REQUEST, GET_SINGLE_FORM_CATEGORY_SUCCESS, SEND_CLOTHING_FORM_FAIL, SEND_CLOTHING_FORM_REQUEST, SEND_CLOTHING_FORM_SUCCESS, SEND_MEAL_FORM_FAIL, SEND_MEAL_FORM_REQUEST, SEND_MEAL_FORM_SUCCESS, SEND_SHELTER_FORM_FAIL, SEND_SHELTER_FORM_REQUEST, SEND_SHELTER_FORM_SUCCESS } from "../constants/FormConstants";
+import { DELETE_CLOTHING_FORM_FAIL, DELETE_CLOTHING_FORM_REQUEST, DELETE_CLOTHING_FORM_SUCCESS, DELETE_MEAL_FORM_FAIL, DELETE_MEAL_FORM_REQUEST, DELETE_MEAL_FORM_SUCCESS, DELETE_SHELTER_FORM_FAIL, DELETE_SHELTER_FORM_REQUEST, DELETE_SHELTER_FORM_SUCCESS, GET_ALL_CLOTHING_FORM_FAIL, GET_ALL_CLOTHING_FORM_REQUEST, GET_ALL_CLOTHING_FORM_SUCCESS, GET_ALL_MEAL_FORM_FAIL, GET_ALL_MEAL_FORM_REQUEST, GET_ALL_MEAL_FORM_SUCCESS, GET_ALL_SHELTER_FORM_FAIL, GET_ALL_SHELTER_FORM_REQUEST, GET_ALL_SHELTER_FORM_SUCCESS, GET_FORM_CATEGORIES_FAIL, GET_FORM_CATEGORIES_REQUEST, GET_FORM_CATEGORIES_SUCCESS, GET_SINGLE_FORM_CATEGORY_FAIL, GET_SINGLE_FORM_CATEGORY_REQUEST, GET_SINGLE_FORM_CATEGORY_SUCCESS, SEND_CLOTHING_FORM_FAIL, SEND_CLOTHING_FORM_REQUEST, SEND_CLOTHING_FORM_SUCCESS, SEND_MEAL_FORM_FAIL, SEND_MEAL_FORM_REQUEST, SEND_MEAL_FORM_SUCCESS, SEND_SHELTER_FORM_FAIL, SEND_SHELTER_FORM_REQUEST, SEND_SHELTER_FORM_SUCCESS } from "../constants/FormConstants";
 
 export const AllFormCategory = () => async (dispatch) => {
     try {
@@ -115,3 +115,140 @@ export const AllFormCategory = () => async (dispatch) => {
     }
   };
   
+
+
+  export const GetAllClothingForm = (userId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: GET_ALL_CLOTHING_FORM_REQUEST,
+      });
+  
+      const { data } = await axios.get(
+        `https://afetapi.onrender.com/api/users/${userId}/clothingForms`
+      );
+  
+      dispatch({
+        type: GET_ALL_CLOTHING_FORM_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_ALL_CLOTHING_FORM_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+
+  export const DeleteClothingForm = (userId,clothingFormId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: DELETE_CLOTHING_FORM_REQUEST,
+      });
+  
+      const { data } = await axios.delete(
+        `https://afetapi.onrender.com/api/users/${userId}/clothingNeedForms/${clothingFormId}/delete`
+      );
+  
+      dispatch({
+        type: DELETE_CLOTHING_FORM_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: DELETE_CLOTHING_FORM_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+
+  export const GetAllShelterForm = (userId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: GET_ALL_SHELTER_FORM_REQUEST,
+      });
+  
+      const { data } = await axios.get(
+        `https://afetapi.onrender.com/api/users/${userId}/shelterForms`
+      );
+  
+      dispatch({
+        type: GET_ALL_SHELTER_FORM_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_ALL_SHELTER_FORM_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+
+  export const DeleteShelterForm = (userId,shelterFormId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: DELETE_SHELTER_FORM_REQUEST,
+      });
+  
+      const { data } = await axios.delete(
+        `https://afetapi.onrender.com/api/users/${userId}/shelterNeedForms/${shelterFormId}/delete`
+      );
+  
+      dispatch({
+        type: DELETE_SHELTER_FORM_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: DELETE_SHELTER_FORM_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+  export const GetAllMealForm = (userId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: GET_ALL_MEAL_FORM_REQUEST,
+      });
+  
+      const { data } = await axios.get(
+        `https://afetapi.onrender.com/api/users/${userId}/mealForms`
+      );
+  
+      dispatch({
+        type: GET_ALL_MEAL_FORM_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_ALL_MEAL_FORM_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+
+  export const DeleteMealForm = (userId,mealFormId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: DELETE_MEAL_FORM_REQUEST,
+      });
+  
+      const { data } = await axios.delete(
+        `https://afetapi.onrender.com/api/users/${userId}/mealNeedForms/${mealFormId}/delete`
+      );
+  
+      dispatch({
+        type: DELETE_MEAL_FORM_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: DELETE_MEAL_FORM_FAIL,
+        error: error.response,
+      });
+    }
+  };
