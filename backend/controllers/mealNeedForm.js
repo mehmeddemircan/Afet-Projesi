b1cee2db698e91676a271400bf96b49ffa072547
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-16 16:16:31 +0300

    meal need form controller changed json

--- backend/controllers/mealNeedForm.js
+++ backend/controllers/mealNeedForm.js
@@ -108,7 +108,7 @@ exports.getUserMealForms = catchAsyncErrors(async(req,res) => {
   try {
 
     const user = await User.findById(req.params.userId).populate("mealForms")
-    res.status(200).json({ mealForms : user.mealForms})
+    res.status(200).json( user.mealForms)
   } catch (error) {
     res.status(500).json({error : error.message})
   }
