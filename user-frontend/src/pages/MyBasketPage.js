7e8fe405783530d59e1c793eb7b541d14fefc54d
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-17 16:16:41 +0300

    created my basket page

---
+++ user-frontend/src/pages/MyBasketPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../components/layout/MainLayout'
+import BasketTabs from '../components/tabs/BasketTabs'
+const MyBasketPage = () => {
+  return (
+    <MainLayout>
+        
+        <BasketTabs />
+    </MainLayout>
+  )
+}
+
+export default MyBasketPage
